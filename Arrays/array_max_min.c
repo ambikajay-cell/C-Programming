9b1d14df07875b2b26a33b9cff1645c371964a91
ambikajay-cell <ammumalu1611@gmail.com>
2026-06-12 22:51:28 +0400

    Create array_max_min.c

---
+++ Arrays/array_max_min.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n, i, arr[100], max, min;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    max = min = arr[0];
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Max = %d\nMin = %d\n", max, min);
+
+    return 0;
+}
