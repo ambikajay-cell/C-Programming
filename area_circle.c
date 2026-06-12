0e341d144118de16d5e5759fac4911816fe6cf04
ambikajay-cell <ammumalu1611@gmail.com>
2026-06-12 22:31:44 +0400

    Create area_circle.c

---
+++ area_circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PI 3.14159
+int main() {
+    float r, area;
+
+    printf("Enter radius: ");
+    scanf("%f", &r);
+
+    area = PI * r * r;
+
+    printf("Area of circle = %.2f\n", area);
+
+    return 0;
+}
